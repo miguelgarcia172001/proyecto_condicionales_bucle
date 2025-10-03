4d87be5f845482bbec5d1d1b3942894b71e810dc
miguelgarcia172001 <palbug02@gmail.com>
2025-10-03 14:12:43 -0500

    Crear archivo condicionales.py en carpeta codigo
    Crear archivo condicionales.py en carpeta codigo

---
+++ codigo/condicionales.py
@@ -0,0 +1,12 @@
+# Ejemplo de condicional simple
+edad = int(input("Ingrese su edad: "))
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+# Ejemplo de bucle while
+i = 0
+while i < 5:
+    print("Iteración:", i)
+    i += 1
